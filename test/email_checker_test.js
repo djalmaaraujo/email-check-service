3814f5c7254c860df60eb29314594a298e72ef9b
djalmaaraujo <djalma.araujo@gmail.com>
2014-07-07 10:57:16 -0300

    email checker improvements

--- test/email_checker_test.js
+++ test/email_checker_test.js
@@ -1,9 +1,60 @@
 var EmailChecker = require('../lib/email_checker');
 var expect = require("chai").expect;
+var fakeDNS = {
+  resolveMX: function (domain, callback) {
+    var servers = [
+    'gmail-smtp-in.l.google.com',
+    'alt2.gmail-smtp-in.l.google.com',
+    'alt4.gmail-smtp-in.l.google.com'];
+
+    callback(null, servers);
+  }
+}
 
 describe("Email Checker", function () {
-  it("expect to receive a email as a parameter", function () {
-    checker = new EmailChecker("mydumb@gmail.com");
-    expect(checker.email).equal("mydumb@gmail.com");
+  var m = {};
+
+  beforeEach(function () {
+    m.checker = new EmailChecker("mydumb@gmail.com");
+  });
+
+  describe("#constructor", function () {
+    it("expect to receive a email as a parameter", function () {
+      expect(m.checker.email).equal("mydumb@gmail.com");
+    });
+  });
+
+  describe("#resolveMX", function () {
+    it("expect to return an empty array of servers for no email", function (done) {
+      dumbChecker = new EmailChecker();
+      dumbChecker.resolveMX(function (servers) {
+        expect(servers).to.be.empty;
+        done();
+      });
+    });
+
+    it("expect to return an array of servers for a valid email", function (done) {
+      m.checker.resolveMX(function (servers) {
+        expect(servers).to.not.be.empty;
+        done();
+      });
+    });
+
+    it("expect to return an array of servers for gmail mx recors", function (done) {
+      m.checker.email = "steve@gmail.com";
+
+      m.checker.resolveMX(function (servers) {
+        expect(servers).to.include({ exchange: 'gmail-smtp-in.l.google.com', priority: 5 });
+        expect(servers).to.include({ exchange: 'alt3.gmail-smtp-in.l.google.com', priority: 30 });
+        expect(servers).to.include({ exchange: 'alt2.gmail-smtp-in.l.google.com', priority: 20 });
+        done();
+      });
+    });
+  });
+
+  describe("#getDomain", function () {
+    it("expect to return the email domain", function () {
+      expect(m.checker.getDomain()).equal("gmail.com");
+    });
   });
 });
