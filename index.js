36df834de3903e9f7816661aa651aa6a1c4b23cd
djalmaaraujo <djalma.araujo@gmail.com>
2014-07-07 16:57:09 -0300

    small fixes - needs refactoring

--- index.js
+++ index.js
@@ -4,21 +4,23 @@ var express = require("express");
 var app = express();
 
 var EmailChecker = require("./lib/email_checker");
-var EmailClient = new EmailChecker("johndoe@gmail.com");
-EmailClient.connection.dnsHandler = dns;
-EmailClient.connection.socketClient = new net.Socket();
 
 app.get("/validate/:email", function(req, res){
-  EmailClient.email = req.params.email;
-  EmailClient.validate().then(function(validation) {
-    res.send(validation);
-  }).fail(function (validation) {
-    res.send(validation);
-  })
+  var EmailClient = new EmailChecker(req.params.email);
+  EmailClient.connection.dnsHandler = dns;
+  EmailClient.connection.socketClient = new net.Socket();
+
+  EmailClient.connect(function () {
+    EmailClient.validate().then(function(validation) {
+      res.send(validation);
+    }).fail(function (validation) {
+      res.send(validation);
+    });
+  });
 });
 
-EmailClient.connect(function () {
-  var server = app.listen(3000, function() {
-    console.log('Listening on port %d', server.address().port);
-  });
+
+var server = app.listen(3000, function() {
+  console.log('Listening on port %d', server.address().port);
+  console.log('To use the API, access /validate/validemail@address.com');
 });
