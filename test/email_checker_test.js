45b2d964ab73bda2c7694cea74e429a2d2732531
djalmaaraujo <djalma.araujo@gmail.com>
2014-07-07 12:28:47 -0300

    refactoring

--- test/email_checker_test.js
+++ test/email_checker_test.js
@@ -1,9 +1,9 @@
-var net = require('net');
-var dns = require('dns');
+var net = require("net");
+var dns = require("dns");
 var expect = require("chai").expect;
-var support = require('./test_helper');
+var support = require("./test_helper");
 var EmailChecker = support.EmailChecker;
-var sinon = require('sinon');
+var sinon = require("sinon");
 
 describe("Email Checker", function () {
   var m = {};
@@ -21,7 +21,7 @@ describe("Email Checker", function () {
       expect(m.checker.connection.dnsHandler).null;
       expect(m.checker.connection.socketClient).null;
       expect(m.checker.connection.port).equal(25);
-      expect(m.checker.connection.domain).equal('gmail.com');
+      expect(m.checker.connection.domain).equal("gmail.com");
     });
 
     it("expect to have edfault informations about the validation", function () {
@@ -57,9 +57,9 @@ describe("Email Checker", function () {
         m.checker.email = "steve@gmail.com";
 
         m.checker.resolveMX(function (servers) {
-          expect(servers).to.include({ exchange: 'gmail-smtp-in.l.google.com', priority: 5 });
-          expect(servers).to.include({ exchange: 'alt3.gmail-smtp-in.l.google.com', priority: 30 });
-          expect(servers).to.include({ exchange: 'alt2.gmail-smtp-in.l.google.com', priority: 20 });
+          expect(servers).to.include({ exchange: "gmail-smtp-in.l.google.com", priority: 5 });
+          expect(servers).to.include({ exchange: "alt3.gmail-smtp-in.l.google.com", priority: 30 });
+          expect(servers).to.include({ exchange: "alt2.gmail-smtp-in.l.google.com", priority: 20 });
           done();
         });
       });
@@ -67,7 +67,7 @@ describe("Email Checker", function () {
 
     describe("#connect", function () {
       it("expect to connect to the socket server using one of the existing servers and port", function (done) {
-        var spy = sinon.spy(m.checker.connection.socketClient, 'connect');
+        var spy = sinon.spy(m.checker.connection.socketClient, "connect");
 
         m.checker.connect();
         expect(spy.calledWith(25, support.servers[0].exchange)).true
@@ -77,7 +77,7 @@ describe("Email Checker", function () {
 
     describe("#destroy", function () {
       it("expect to destroy the client socket", function (done) {
-        var spy = sinon.spy(m.checker.connection.socketClient, 'destroy');
+        var spy = sinon.spy(m.checker.connection.socketClient, "destroy");
 
         m.checker.destroy();
         expect(spy.called).true
